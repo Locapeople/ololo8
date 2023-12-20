c7f1cc1446884318b457e29e3d78ffc1484fe6b2
Locapeople <missis.anyack@yandex.ru>
2023-12-20 12:27:33 +0300

    fix test code

--- src/test/java/ru/netology/web/CardBookingTest.java
+++ src/test/java/ru/netology/web/CardBookingTest.java
@@ -8,9 +8,9 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static java.util.Objects.requireNonNull;
@@ -19,30 +19,30 @@ public class CardBookingTest {
 
     @Test
     void testCorrectInputs() {
-        open("http://0.0.0.0:9999");
-        SelenideElement form = $(".form.form_size_m.form_theme_alfa-on-white");
-        form.$("[data-test-id=city] input").setValue("Уфа");
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Уфа");
         // получить дату указанную в поле (сегодня)
-        String currentDateString = form.$("[data-test-id=date] input").getValue();
-        // сделать из нее объект даты с форматированием
-        DateTimeFormatter f = DateTimeFormatter.ofPattern("dd.MM.uuuu");
-        LocalDate currentDate = LocalDate.parse(requireNonNull(currentDateString), f);
-        // прибавить 5 дней
-        LocalDate newDate = currentDate.plusDays(5);
-        String newDateString = f.format(newDate);
+        String currentDateString = $("[data-test-id=date] input").getValue();
+        // получить новую дату (прибавить 5 дней)
+        String newDateString = dateAdjustDaysStringFormatted(currentDateString, "dd.MM.uuuu", 5);
         // очистить пред. значение
-        // я не знаю, как добраться до простого поля внутри calendar-input,
-        // поэтому удалю старую дату, понажимав backspace 8 раз
-        for(int i = 0; i < 8; i++) {
-            form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        }
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         // записать в поле новую дату
-        form.$("[data-test-id=date] input").setValue(newDateString);
-        form.$("[data-test-id=name] input").setValue("Фамилия Имя");
-        form.$("[data-test-id=phone] input").setValue("+79998887766");
-        form.$("[data-test-id=agreement]").click();
-        form.$(byText("Забронировать")).click();
+        $("[data-test-id=date] input").setValue(newDateString);
+        $("[data-test-id=name] input").setValue("Фамилия Имя");
+        $("[data-test-id=phone] input").setValue("+79998887766");
+        $("[data-test-id=agreement]").click();
+        $(byText("Забронировать")).click();
 
-        $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id=notification]")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Успешно!\nВстреча успешно забронирована на " + newDateString));
+    }
+
+    String dateAdjustDaysStringFormatted(String date, String format, long days) {
+        DateTimeFormatter f = DateTimeFormatter.ofPattern(format);
+        LocalDate currentDate = LocalDate.parse(requireNonNull(date), f);
+        LocalDate newDate = currentDate.plusDays(days);
+        return f.format(newDate);
     }
 }
